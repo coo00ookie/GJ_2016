21a881e94be1fe4119b673b913ff75547f4ab055
박세원 <mostextraordinary@naver.com>
2016-09-24 04:23:04 +0900

    버튼 클릭 및 화면 이동

--- app/src/main/java/com/example/gj/gj2016/TestActivity.java
+++ app/src/main/java/com/example/gj/gj2016/TestActivity.java
@@ -1,7 +1,10 @@
 package com.example.gj.gj2016;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TestActivity extends AppCompatActivity {
 
@@ -9,7 +12,14 @@ public class TestActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
-        //sArOdiN
-        //가
+
+        Button test_button = (Button)findViewById(R.id.button);
+        test_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(TestActivity.this, SamsungPay.class);
+                startActivity(intent);
+            }
+        });
     }
 }
