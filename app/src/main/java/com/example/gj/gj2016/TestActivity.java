1497679b7c9aafc3e3ee92737dbb30603ff50286
박세원 <mostextraordinary@naver.com>
2016-09-24 02:24:45 +0900

    test commit

--- app/src/main/java/com/example/gj/gj2016/TestActivity.java
+++ app/src/main/java/com/example/gj/gj2016/TestActivity.java
@@ -10,5 +10,6 @@ public class TestActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
         //sArOdiN
+        //지제이
     }
 }
