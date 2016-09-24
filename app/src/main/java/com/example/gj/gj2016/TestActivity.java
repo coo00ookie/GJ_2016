a0fe38b790e5f59dd78b2fca330f5c348fa2d1f0
박세원 <mostextraordinary@naver.com>
2016-09-24 12:33:46 +0900

    애니메이션

--- app/src/main/java/com/example/gj/gj2016/TestActivity.java
+++ app/src/main/java/com/example/gj/gj2016/TestActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ViewFlipper;
@@ -13,12 +15,18 @@ public class TestActivity extends AppCompatActivity {
     ViewFlipper flipper;
     float x;
     int curIdx;
+    Animation showIn;
+    Animation showOut;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         getMatchInformation getter = new getMatchInformation();
         getter.parseData("A", "B");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test);
+        showIn= AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left);
+        showOut= AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right);
+
+
         curIdx = 0;
         flipper = (ViewFlipper)findViewById(R.id.flipper);
         flipper.setOnTouchListener(new View.OnTouchListener() {
@@ -41,11 +49,15 @@ public class TestActivity extends AppCompatActivity {
                                break;
                            }
                            if(x <= event.getX()) {
+                               flipper.setInAnimation(showIn);
+                                flipper.setOutAnimation(showOut);
                                flipper.showPrevious();
                                curIdx--;
                                if(curIdx <0) curIdx = 2;
                            }
                            else if(x > event.getX()) {
+                               flipper.setInAnimation(showIn);
+                                flipper.setOutAnimation(showOut);
                                flipper.showNext();
                                curIdx++;
                                if(curIdx >2) curIdx = 0;
